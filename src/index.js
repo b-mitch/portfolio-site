293f87a8745a0a7c5bd2575a176c1e9c716e14dc
b-mitch <brendenmitchum@gmail.com>
2023-05-04 14:14:55 -0700

    walk back before pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { HashRouter as React } from 'react';import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
