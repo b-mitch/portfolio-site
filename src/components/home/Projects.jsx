f926336fb0ae36f5b551d6ea44595dc8be99effe
b-mitch <bmitchum.dev@gmail.com>
2024-01-22 10:22:56 -0500

    update woutfh link

--- src/components/home/Projects.jsx
+++ src/components/home/Projects.jsx
@@ -16,7 +16,7 @@ export default function Projects({ projectsStyles }) {
         <h2 style={altStyle} className="grow2">Projects</h2><br/>
           <ul>
             <li><a href="https://github.com/b-mitch/buymystuff" target="_blank" rel="noreferrer">Buy My Stuff - e-commerce app</a></li>
-            <li><a href="http://54.196.137.157/" target="_blank" rel="noreferrer">Workout From Home - home workouts & scheduling app</a></li>
+            <li><a href="http://www.woutfh.com/" target="_blank" rel="noreferrer">Workout From Home - home workouts & scheduling app</a></li>
             <li><a href="https://github.com/b-mitch/minesweeper" target="_blank" rel="noreferrer">Minesweeper - console game</a></li>
           </ul><br/><br/>
         </div>
