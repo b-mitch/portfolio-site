a3ff788e5e3f5af782cafe2e47cdc2f0c852d108
b-mitch <bmitchum.dev@gmail.com>
2024-01-29 09:17:42 -0500

    updates skills

--- src/components/home/Skills.jsx
+++ src/components/home/Skills.jsx
@@ -20,8 +20,8 @@ export default function Skills({ skillsStyles }) {
             <i className="fa-brands fa-square-js fa-5x"></i>
             <i className="fa-brands fa-react fa-5x"></i>
             <i className="fa-brands fa-node-js fa-5x"></i>
-            <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="f7dd5387-7045-4b53-9e6f-b1260bde9380" data-share-badge-host="https://www.credly.com"></div>
-            <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="08d815ee-d932-4e46-ac18-65f7a5a816a6" data-share-badge-host="https://www.credly.com"></div>
+            <div dataIframeWidth="150" dataIframeHeight="270" dataShareBadgeId="f7dd5387-7045-4b53-9e6f-b1260bde9380" dataShareBadgeHost="https://www.credly.com"></div>
+            <div dataIframeWidth="150" dataIframeHeight="270" dataShareBadgeId="08d815ee-d932-4e46-ac18-65f7a5a816a6" dataShareBadgeHost="https://www.credly.com"></div>
             <i class="fa-brands fa-docker fa-5x"></i>
           </div>
         </div>
