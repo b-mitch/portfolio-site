a42ab44dce2c8a51666edbbc461665984e37ecb5
Brenden Mitchum <bmitchum.dev@gmail.com>
2025-12-26 12:37:30 -0500

    add certificates to navbar

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -6,26 +6,29 @@ interface NavBarProps { handleClick: (e: React.MouseEvent) => void }
 
 export default function NavBar({ handleClick }: NavBarProps): JSX.Element {
     return (
-    <div className='header'>
-        <nav>
-        <div className="heading">
-            <h2>Brenden Mitchum</h2>
+        <div className='header'>
+            <nav>
+            <div className="heading">
+                <h2>Brenden Mitchum</h2>
+            </div>
+            <ul>
+                <li>
+                <NavHashLink smooth to="/#about" className="click1" onClick={handleClick}>About Me</NavHashLink>
+                </li>
+                <li>
+                <NavHashLink smooth to="/#projects" className="click2" onClick={handleClick}>Projects</NavHashLink>
+                </li>
+                <li>
+                <NavHashLink smooth to="/#skills" className="click3" onClick={handleClick}>Skills</NavHashLink>
+                </li>
+                <li>
+                <NavHashLink smooth to="/#certificates">Certificates</NavHashLink>
+                </li>
+                <li>
+                <NavLink to="/contact">Contact Me</NavLink>
+                </li>
+            </ul>
+            </nav>
         </div>
-        <ul>
-            <li>
-            <NavHashLink smooth to="/#about" className="click1" onClick={handleClick}>About Me</NavHashLink>
-            </li>
-            <li>
-            <NavHashLink smooth to="/#projects" className="click2" onClick={handleClick}>Projects</NavHashLink>
-            </li>
-            <li>
-            <NavHashLink smooth to="/#skills" className="click3" onClick={handleClick}>Skills</NavHashLink>
-            </li>
-            <li>
-            <NavLink to="/contact">Contact Me</NavLink>
-            </li>
-        </ul>
-        </nav>
-    </div>
-    )
-  }
+    );
+}
