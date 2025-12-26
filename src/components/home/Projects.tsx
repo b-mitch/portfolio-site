a75e5bab69edba787a0f1ef23b5541fc05e56639
Brenden Mitchum <bmitchum.dev@gmail.com>
2025-12-26 11:25:26 -0500

    update links to make sure all working

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -17,9 +17,9 @@ export default function Projects({ projectsStyles }: ProjectsProps): JSX.Element
         <div id="projects" className="projects">
         <h2 style={altStyle} className="grow2">Projects</h2><br/>
           <ul>
-            <li><a href="https://github.com/b-mitch/buymystuff" target="_blank" rel="noreferrer">Buy My Stuff - e-commerce app</a></li>
-            <li><a href="http://www.woutfh.com/" target="_blank" rel="noreferrer">Workout From Home - home workouts & scheduling app</a></li>
-            <li><a href="https://github.com/b-mitch/minesweeper" target="_blank" rel="noreferrer">Minesweeper - console game</a></li>
+            <li><a href="https://github.com/b-mitch/buymystuff/" target="_blank" rel="noreferrer">Buy My Stuff - e-commerce app</a></li>
+            <li><a href="https://github.com/b-mitch/woutfh_prod/" target="_blank" rel="noreferrer">Workout From Home - home workouts & scheduling app</a></li>
+            <li><a href="https://github.com/b-mitch/minesweeper/" target="_blank" rel="noreferrer">Minesweeper - console game</a></li>
           </ul><br/><br/>
         </div>
     )
