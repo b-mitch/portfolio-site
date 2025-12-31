ffc36b2300ffdedb77b31204e8ca3908af3032c7
Brenden Mitchum <bmitchum.dev@gmail.com>
2025-12-31 10:46:32 -0500

    update skills and add github link to contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 // TODO: replace with your actual LinkedIn URL
 const LINKEDIN_URL = 'https://www.linkedin.com/in/brenden-mitchum/';
+const GITHUB_URL = 'https://github.com/b-mitch/'
 
 export default function Contact(): JSX.Element {
     return (
@@ -15,6 +16,10 @@ export default function Contact(): JSX.Element {
                 <a href={LINKEDIN_URL} target="_blank" rel="noopener noreferrer" aria-label="View LinkedIn profile" title="LinkedIn">
                     <i className="fa-brands fa-linkedin fa-3x" aria-hidden="true"></i>
                     <span className="sr-only">LinkedIn</span>
+                </a>
+                <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer" aria-label="View GitHub profile" title="GitHub">
+                    <i className="fa-brands fa-github fa-3x" aria-hidden="true"></i>
+                    <span className="sr-only">GitHub</span>
                 </a>
             </div>
         </div>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,14 +6,26 @@ export default function Skills(): JSX.Element {
         <div id="skills" className="section">
           <h1>Skills</h1><br/>
           <div className="badge-container">
-            <i className="fa-brands fa-html5 fa-5x"></i>
-            <i className="fa-brands fa-css3-alt fa-5x"></i>
-            <i className="fa-brands fa-square-js fa-5x"></i>
-            <i className="fa-brands fa-react fa-5x"></i>
-            <i className="fa-brands fa-node-js fa-5x"></i>
-            <i className="fa-brands fa-python fa-5x"></i>
-            <i className="fa-brands fa-aws fa-5x"></i>
-            <i className="fa-brands fa-docker fa-5x"></i>
+            <i className="fa-brands fa-github fa-5x" title="GitHub"></i>
+            <i className="fa-brands fa-linux fa-5x" title="Linux"></i>
+            <i className="fa-brands fa-react fa-5x" title="React"></i>
+            <i className="fa-brands fa-node-js fa-5x" title="Node.js"></i>
+            <i className="fa-brands fa-python fa-5x" title="Python"></i>
+            <i className="fa-brands fa-aws fa-5x" title="AWS"></i>
+            <i className="fa-brands fa-docker fa-5x" title="Docker"></i>
+            <i className="fa-brands fa-jenkins fa-5x" title="Jenkins"></i>
+            <svg className="svg-icons" aria-labelledby="pulumi-title" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <title id="pulumi-title">Pulumi</title>
+              <path id="pulumi"/>
+            </svg>
+            <svg className="svg-icons" aria-labelledby='ts-title' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+              <title id="ts-title">TypeScript</title>
+              <path id="ts"/>
+            </svg>
+            <svg className="svg-icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+              <title id="terraform-title">Terraform</title>
+              <path id="terraform"/>
+            </svg>
           </div>
         </div>
     )
