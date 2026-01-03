2f875f2112f7a93a57c09d487ca928bde42412a4
Brenden Mitchum <bmitchum.dev@gmail.com>
2026-01-03 10:42:20 -0500

    configure for gh pages managed branch deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  // use a repo-relative base when building for GitHub Pages
+  base: mode === 'production' ? '/portfolio-site/' : '/',
   plugins: [react()],
-})
+}))
